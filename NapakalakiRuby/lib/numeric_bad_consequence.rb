09924f1329ddd5bbdf35b532c7dc52c79eab74d0
Daniel Bolaños <bolanosmartinezdaniel@gmail.com>
2017-05-04 11:49:06 +0200

    Ruby actualizado

--- NapakalakiRuby/lib/numeric_bad_consequence.rb
+++ NapakalakiRuby/lib/numeric_bad_consequence.rb
@@ -19,16 +19,9 @@ module NapakalakiGame
     end
   
     def substractVisibleTreasure(t)
-      n_visible = @nVisibleTreasures
-      n_hidden = @nHiddenTreasures
-      if v.size < n_visible then
-        n_visible = v.size
+      if @nVisibleTreasures > 0 then
+        @nVisibleTreasures -= 1
       end
-      if h.size < n_hidden then
-      n_hidden = h.size
-      end
-      bc = NumericBadConsequence.new(@text, @levels, n_visible, n_hidden)
-      bc
     end 
     
     def substractHiddenTreasure(t)
