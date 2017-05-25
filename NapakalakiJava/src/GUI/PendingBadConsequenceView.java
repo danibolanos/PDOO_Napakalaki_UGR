f2e1056b97df3b10c47392b6de85be332add8440
jomabose <jomabose@gmail.com>
2017-05-25 11:45:05 +0200

    IA ETA

--- NapakalakiJava/src/GUI/PendingBadConsequenceView.java
+++ NapakalakiJava/src/GUI/PendingBadConsequenceView.java
@@ -22,23 +22,46 @@ public class PendingBadConsequenceView extends javax.swing.JPanel {
      */
     public PendingBadConsequenceView() {
         initComponents();
+           setVisibleSpecific(false);
+           setVisibleNumeric(false);
     }
     
     public void setPendingBadConsequence(BadConsequence bc){
        badConsequenceModel = bc;
        
        if(badConsequenceModel instanceof NumericBadConsequence){
+           setVisibleNumeric(true);
+           setVisibleSpecific(false);
           this.randomVT.setText(Integer.toString(((NumericBadConsequence)badConsequenceModel).getNVisibleTreasures()));
           this.randomHT.setText(Integer.toString(((NumericBadConsequence)badConsequenceModel).getNHiddenTreasures()));
        }
        
        if(badConsequenceModel instanceof SpecificBadConsequence){
+           setVisibleNumeric(false);
+           setVisibleSpecific(true);
           this.specificVT.setText(((SpecificBadConsequence)badConsequenceModel).getSpecificVisibleTreasures().toString());
           this.specificHT.setText(((SpecificBadConsequence)badConsequenceModel).getSpecificHiddenTreasures().toString());
        }
        
        repaint();
     }
+    
+    public void setVisibleNumeric(boolean set){
+        nLabel1.setVisible(set);
+        nLabel2.setVisible(set);
+        nLabel3.setVisible(set);
+        randomHT.setVisible(set);
+        randomVT.setVisible(set);
+    }
+    
+    public void setVisibleSpecific(boolean set){
+        sLabel1.setVisible(set);
+        sLabel2.setVisible(set);
+        sLabel3.setVisible(set);
+        specificHT.setVisible(set);
+        specificVT.setVisible(set);
+    }
+            
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -49,34 +72,34 @@ public class PendingBadConsequenceView extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
-        jLabel9 = new javax.swing.JLabel();
+        nLabel1 = new javax.swing.JLabel();
+        sLabel1 = new javax.swing.JLabel();
+        nLabel2 = new javax.swing.JLabel();
+        sLabel2 = new javax.swing.JLabel();
+        nLabel3 = new javax.swing.JLabel();
+        sLabel3 = new javax.swing.JLabel();
         randomVT = new javax.swing.JLabel();
         specificVT = new javax.swing.JLabel();
         randomHT = new javax.swing.JLabel();
         specificHT = new javax.swing.JLabel();
 
-        jLabel4.setFont(new java.awt.Font("DejaVu Sans", 1, 12)); // NOI18N
-        jLabel4.setText("Tesoros Visibles perdidos:");
+        nLabel1.setFont(new java.awt.Font("DejaVu Sans", 1, 12)); // NOI18N
+        nLabel1.setText("Tesoros Visibles perdidos:");
 
-        jLabel5.setFont(new java.awt.Font("DejaVu Sans", 1, 12)); // NOI18N
-        jLabel5.setText("Tesoros Ocultos perdidos:");
+        sLabel1.setFont(new java.awt.Font("DejaVu Sans", 1, 12)); // NOI18N
+        sLabel1.setText("Tesoros Ocultos perdidos:");
 
-        jLabel6.setFont(new java.awt.Font("DejaVu Sans", 1, 12)); // NOI18N
-        jLabel6.setText("Aleatorios:");
+        nLabel2.setFont(new java.awt.Font("DejaVu Sans", 1, 12)); // NOI18N
+        nLabel2.setText("Aleatorios:");
 
-        jLabel7.setFont(new java.awt.Font("DejaVu Sans", 1, 12)); // NOI18N
-        jLabel7.setText("Aleatorios:");
+        sLabel2.setFont(new java.awt.Font("DejaVu Sans", 1, 12)); // NOI18N
+        sLabel2.setText("Aleatorios:");
 
-        jLabel8.setFont(new java.awt.Font("DejaVu Sans", 1, 12)); // NOI18N
-        jLabel8.setText("Específicos:");
+        nLabel3.setFont(new java.awt.Font("DejaVu Sans", 1, 12)); // NOI18N
+        nLabel3.setText("Específicos:");
 
-        jLabel9.setFont(new java.awt.Font("DejaVu Sans", 1, 12)); // NOI18N
-        jLabel9.setText("Específicos:");
+        sLabel3.setFont(new java.awt.Font("DejaVu Sans", 1, 12)); // NOI18N
+        sLabel3.setText("Específicos:");
 
         randomVT.setText("randomVT");
 
@@ -92,26 +115,23 @@ public class PendingBadConsequenceView extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(nLabel1)
+                    .addComponent(sLabel1)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(42, 42, 42)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel4)
-                            .addComponent(jLabel5)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel6)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(randomVT, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addComponent(jLabel8)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(specificVT, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel7)
-                                .addGap(18, 18, 18)
-                                .addComponent(randomHT, javax.swing.GroupLayout.PREFERRED_SIZE, 353, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                        .addComponent(nLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(randomVT, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(nLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(specificVT, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(43, 43, 43)
-                        .addComponent(jLabel9)
+                        .addComponent(sLabel2)
+                        .addGap(18, 18, 18)
+                        .addComponent(randomHT, javax.swing.GroupLayout.PREFERRED_SIZE, 353, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(1, 1, 1)
+                        .addComponent(sLabel3)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(specificHT, javax.swing.GroupLayout.PREFERRED_SIZE, 334, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -119,38 +139,37 @@ public class PendingBadConsequenceView extends javax.swing.JPanel {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel4)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel6)
-                    .addComponent(randomVT, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel8)
-                    .addComponent(specificVT, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel5)
+                .addComponent(nLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel7)
+                    .addComponent(nLabel2)
+                    .addComponent(randomVT, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(nLabel3)
+                    .addComponent(specificVT, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(sLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(sLabel2)
                     .addComponent(randomHT, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel9)
+                    .addComponent(sLabel3)
                     .addComponent(specificHT, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18))
+                .addGap(0, 0, 0))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JLabel jLabel9;
+    private javax.swing.JLabel nLabel1;
+    private javax.swing.JLabel nLabel2;
+    private javax.swing.JLabel nLabel3;
     private javax.swing.JLabel randomHT;
     private javax.swing.JLabel randomVT;
+    private javax.swing.JLabel sLabel1;
+    private javax.swing.JLabel sLabel2;
+    private javax.swing.JLabel sLabel3;
     private javax.swing.JLabel specificHT;
     private javax.swing.JLabel specificVT;
     // End of variables declaration//GEN-END:variables
